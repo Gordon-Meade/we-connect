b380187e43e8851b09eb69acc6a2bfc70e60b073
piya-ds Savalge <priyanka.savalge94@gmail.com>
2024-03-06 10:41:32 +0000

    super user created

--- weconnect/settings.py
+++ weconnect/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-w2qv&r4lu5f&a&#*yi2u*o!$y+c3f98o%+ls#3sm2-tj%ujp(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['http://8000-piyads-weconnect-r4vi6dt0agu.ws-eu108.gitpod.io/','http://8000-gordonmeade-weconnect-0hef7y3py4j.ws-eu108.gitpod.io/','https://5432-mel0dramat1c-weconnect-52xeszz068c.ws-eu108.gitpod.io/','8000-piyads-weconnect-r4vi6dt0agu.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -91,6 +91,7 @@ DATABASES = {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 CSRF_TRUSTED_ORIGINS = [
 <<<<<<< HEAD
     "https://*.herokuapp.com",
@@ -103,6 +104,13 @@ CSRF_TRUSTED_ORIGINS = [
 
 =======
 >>>>>>> cabc0fa (Create Event App)
+=======
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
+>>>>>>> 5894c04 (super user created)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
