43ffe7da67a49da6eab7e9deabe5add87c643b37
piya-ds Savalge <priyanka.savalge94@gmail.com>
2024-03-07 10:02:16 +0000

    review model added

--- event/models.py
+++ event/models.py
@@ -1,6 +1,8 @@
 from django_summernote.fields import SummernoteTextField
 from django.db import models
 
+RATING = ((1, "Highly Dissatisfied"), (2, "Dissatisfied"),(3,"Neutral"),(4,"Satisfied")(5,"Highly Satisfied"))
+
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -13,5 +15,25 @@ class Event(models.Model):
     review = models.TextField(blank=True, null=True)
     rating = models.PositiveIntegerField(blank=True, null=True)
 
+     class Meta:
+        ordering = ["-date"]
+
     def __str__(self):
         return self.title
+
+
+# class Review(models.Model):
+#     title = models.ForeignKey(
+#         Event, on_delete=models.CASCADE, related_name="event-title")
+#     user = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="user_name")
+#     reviewbody = models.TextField()
+#     rating = models.IntegerField(choices=RATING, default=5)
+#     approved = models.BooleanField(default=False)
+#     created_on = models.DateTimeField(auto_now_add=True)
+
+#      class Meta:
+#         ordering = ["-rating"]
+
+#     def __str__(self):
+#         return f"Review {self.reviewbody} by {self.user}"
